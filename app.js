a5b5b5e134d04210dc685aba7e8a8f84439afc0a
Marvin Macharia <machariamarvin625@gmail.com>
2022-02-09 16:00:28 +0300

    Create intro paragraph and explore button

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
+var myVideo = document.getElementById("header");
+var explorebtn = document.querySelector(".explore-button");
+var spanAnimation = document.querySelector(".span");
+var children = spanAnimation.childNodes;
 
-var myVideo = document.getElementById("header")
 myVideo.addEventListener("ended", function() {
     myVideo.autoplay = false;
     myVideo.load();
 })
+
+var i = 0;
+var intro = "We are  Ewa Air Jets Solutions."
+var introTitle = document.getElementsByClassName("header-tag-title");
+var introHead = document.querySelector("#header-tag1-paragraph");
+var speed = 90;
+
+function spanRemove() {
+    spanAnimation.classList.add("span-hover");
+}
+
+function spanAppend() {
+    spanAnimation.classList.remove("span-hover");
+}
+
+function typing() {
+    if (i < intro.length) {
+        $(introTitle).fadeIn();
+        introHead.innerHTML += intro.charAt(i);
+        i++;
+        setTimeout(typing, speed)
+    }
+
+    else if (i = intro.length) {
+        $(explorebtn).slideDown();
+    }
+}
+
+
 
 $(function() {
     $("#navbar-toggler-icon").click(function () {
@@ -20,4 +52,5 @@ $(function() {
         $("#dropdown2").slideToggle();
         $("#dropdown1").slideUp();
     })
+
 })
