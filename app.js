1cf1ce831d1fa53da0f83654e90728e5d19767cd
Marvin Macharia <machariamarvin625@gmail.com>
2022-02-09 23:33:44 +0300

    Create feedback form

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 var myVideo = document.getElementById("header");
-var explorebtn = document.querySelector(".explore-button");
-var spanAnimation = document.querySelector(".span");
-var children = spanAnimation.childNodes;
 
-myVideo.addEventListener("ended", function() {
+myVideo.addEventListener("ended", function () {
     myVideo.autoplay = false;
     myVideo.load();
 })
 
-var i = 0;
-var intro = "We are  Ewa Air Jets Solutions."
-var introTitle = document.getElementsByClassName("header-tag-title");
-var introHead = document.querySelector("#header-tag1-paragraph");
-var speed = 90;
+var explorebtn = document.querySelector(".explore-button");
+var spanAnimation = document.querySelector(".span");
+var children = spanAnimation.childNodes;
 
 function spanRemove() {
     spanAnimation.classList.add("span-hover");
@@ -22,35 +17,100 @@ function spanAppend() {
     spanAnimation.classList.remove("span-hover");
 }
 
+var i = 0;
+var intro = "We are  Ewa Air Jets Solutions."
+var introTitle = document.getElementsByClassName("header-tag-title");
+var introHead = document.querySelector("#header-tag1-paragraph");
+var speed = 90;
+
+var feedbackForm = document.querySelector(".feedback-form");
+
 function typing() {
     if (i < intro.length) {
         $(introTitle).fadeIn();
         introHead.innerHTML += intro.charAt(i);
         i++;
         setTimeout(typing, speed)
-    }
-
-    else if (i = intro.length) {
+    } else if (i = intro.length) {
         $(explorebtn).slideDown();
+        $(feedbackForm).slideDown();
     }
 }
 
 
 
-$(function() {
+$(function () {
     $("#navbar-toggler-icon").click(function () {
         $("#navbarSupportedContent").slideToggle();
-        $(".form").css({"margin":"auto"})
+        $(".form").css({
+            "margin": "auto"
+        })
     })
 
-    $("#privateDropdown").click(function() {
+    $("#privateDropdown").click(function () {
         $("#dropdown1").slideToggle();
         $("#dropdown2").slideUp();
     })
 
-    $("#airDropdown").click(function() {
+    $("#airDropdown").click(function () {
         $("#dropdown2").slideToggle();
         $("#dropdown1").slideUp();
     })
 
+    $('input').blur(function () {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
+    });
+
+    var $ripples = $('.ripples');
+
+    $ripples.on('click.Ripples', function (e) {
+
+        var $this = $(this);
+        var $offset = $this.parent().offset();
+        var $circle = $this.find('.ripplesCircle');
+
+        var x = e.pageX - $offset.left;
+        var y = e.pageY - $offset.top;
+
+        $circle.css({
+            top: y + 'px',
+            left: x + 'px'
+        });
+
+        $this.addClass('is-active');
+
+    });
+
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function (e) {
+        $(this).removeClass('is-active');
+    });
+
+    $(feedbackForm).submit(function(event) {
+        event.preventDefault();
+
+        var name = document.getElementById("name");
+        var message = document.getElementById("message");
+
+        if ((name.value == null || name.value == "") && (message.value == null || message.value == "")) {
+            name.style.borderBottom = "1px solid red";
+            message.style.borderBottom = "1px solid red";
+        }
+
+        else if (name.value == null || name.value == "") {
+            name.style.borderBottom = "1px solid red";
+        }
+
+        else if (message.value == null || message.value == "") {
+            message.style.borderBottom = "1px solid red"
+        }
+
+        else {
+            name.style.borderBottom = "#757575";
+            message.style.borderBottom = "#757575"
+        }
+    })
 })
